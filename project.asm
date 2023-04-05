3c776199847d3b185b91fa51c45739eabdf1323d
IshayHarmatz <117118962+IshayHarmatz@users.noreply.github.com>
2023-04-05 16:06:39 +0300

    project assembly

---
+++ project.asm
@@ -0,0 +1,55 @@
+.model small
+.stack 100h
+.data
+Filename db 'test.txt',0
+filehandle dw ?
+NumOfBytes dw 10 
+Buffer db 200 dup (0) 
+strlen db 0
+.code
+start:
+      mov ax,@data
+      mov ds,ax 
+      
+      ;open file
+      mov ah,3dh
+      xor al,al
+      lea dx,[Filename]
+      int 21h
+      ;cmp ax,0
+      ;jne exit
+      mov [filehandle],ax
+      
+      ;read file
+      mov ah,3fh
+      mov bx,[filehandle]
+      mov cx,NumOfBytes
+      mov dx,offset Buffer  
+      int 21h  
+      
+      ;put $ at the end
+      xor ax,ax
+      mov ax,NumOfBytes
+      lea bx,Buffer
+      add bx,ax
+      mov [bx],'$'       
+      
+      ;print file
+      lea dx,Buffer
+      mov ah,9h 
+      int 21h   
+      
+exit:
+mov ah,4ch
+int 21h
+end      
+       
+      
+      
+      
+      
+      
+
+
+
+
