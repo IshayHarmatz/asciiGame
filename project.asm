bed345f1ebf9289f67aba21ad6f2c938df42dcff
IshayHarmatz <117118962+IshayHarmatz@users.noreply.github.com>
2023-05-31 23:08:58 +0300

    פרויקט סופי

--- project.asm
+++ project.asm
@@ -1,20 +1,26 @@
 .model small
 .stack 100h
 .data     
-msg1 db 13,10,'Enter a file name or zero to end write files:',13,10,'$' ;to print a msg for get filename input
-msg2 db 13,10,'Enter num of characters that you want to see {more}',13,10,'$' ;to print a msg for get number of characters to print from a file input
+msg1 db 13,10,'Enter a file name or 0 to end write files:',13,10,'$' ;to print a msg for get filename input
+msg2 db 13,10,'Enter num of characters that you want to see {more} until 9999 characters',13,10,'$' ;to print a msg for get number of characters to print from a file input
 msg3 db 13,10,'Guess which file it is :',13,10,'$';to print a msg for get guess of a file name
 msg4 db 13,10,'Well done! You were right',13,10,'$' ;to print a msg that say you guess the right file
-msg5 db 13,10,'You failed! Do you wan to get more characters?{yes or no}',13,10,'$'; to say you wrong and ask if yow want keep plaing        
+msg5 db 13,10,'You failed!',13,10,'$'; to say you wrong and ask if yow want keep plaing        
 msg6 db 13,10,'Enter any key to exit:','$' ;to end the program
-msg7 db 'If you want to read the game instructions enter yes:',13,10,'$'; ask if yoy want see the instructions
-msg8 db 13,10,'At first you enter up to 10 file names.$'; the instuctions
-msg9 db 13,10,'Then you write how many characters you want to see from the file.$'  ; the instuctions
-msg10 db 13,10,'At the end you guess which file it is. If you are right the game is over.$' ; the instuctions
-msg11 db 13,10,'If you are wrong you can continue plaing and get more characters from the file.',13,10,'$'; the instuctions 
-instr db 100  ; to get the answer if he want to see the instructions
+msg7 db 'Enter yes to see the instructions and other key to start the game:',13,10,'$'; ask if you want see the instructions
+msg8 db 13,10,'At first you enter up to 10 file names.'; the instuctions
+     db 13,10,'Then you write how many characters you want to see from the file.'  ; the instuctions
+     db 13,10,'At the end you guess which file it is. If you are right the game is over.' ; the instuctions
+     db 13,10,'If you are wrong you can continue plaing and get more characters.'; the instuctions
+     db 13,10,'If you Guessed wrong and saw all the characters the game is over!',13,10,'$'  
+msg9 db 13,10,'Invalid Number! Try again$'; erorr input number msg
+msg10 db 13,10,'The game is over because there is no file input at all!$'; when there is no file input from user 
+msg11 db 13,10,'The program printed all the characters so the game is over!$'; when there is no file input from user 
+msg12 db 'Enter yes for keep playing and other thing to over the game',13,10,'$'; ask if the user want to keep playing
+msg13 db 'Sorry but the program can not open the file, the game is over',13,10,'$'
+instr db 4  ; to get the answer if he want to see the instructions  
 instrlen db 0 ; the length of the answer
-instrAnswer dw 100 dup (0)  ; the answer is placed here
+instrAnswer dw 4 dup (0)  ; the answer is placed here
 returnAddress dw ? ; to return from a function to the ip place
 filehandle dw ?  ; to see if you can print a file
 NumOfBytes dw 10  ; The amount of characters that the program print from a file
@@ -53,21 +59,23 @@ Filename9 db 100 dup(0) ; the name of file 9
 str10 db 100   ; to get file name 10
 strlen10 db 0   ; The len of file name 10
 Filename10 db 100 dup(0) ; the name of file 10
-strbyte db 100 ; to get the num of bytes to print from the file
+strbyte db 5 ; to get the num of bytes to print from the file
 strbylen db 0   ; the length of the input (num of byte) 
-bytes db 100 dup(0); the num of bytes to print from file
+bytes db 5 dup(0); the num of bytes to print from file
 strGuss db 100    ; to get the user guess of the file name 
 strGusslen db 0   ; the length of the user guess
 FileGuess db 100 dup(0) ; the guess witch file it is;
-strAn db 100  ;  to get the user answer if he wants to keep playing
+strAn db 4  ;  to get the user answer if he wants to keep playing
 Anlen7 db 0   ; the answer length
-Answer dw 100 dup(0) ;  the answer of the user if he wants to keep playing
+Answer dw 4 dup(0) ;  the answer of the user if he wants to keep playing
 totalBytes dw 100 dup(0) ; the total bytes (from the start of the program) that need to print from the file   
 mone dw ? ; the number to print 10 characters from the file
 macpil dw 1 ; to convert micidot to asarot and meot...
 sherit dw ? ; to keep the sherit of bytes that need to print and it less then 10
 result dw 0 ; the number of bytes that need to print after the convert
-Yes dw 'yes' ; check if the user want keep playing 
+Yes dw 'yes' ; check if the user want keep playing or see the instructions
+fileAddres dw 0 ; to keep the offset of the random file
+ 
 .code  
 
 start:
@@ -92,8 +100,12 @@ startGame:
       push offset strlen
       push offset Filename
       
-      call GetFiles ;get files
-      mov bx,cx 
+      call GetFiles ;get files 
+      
+      cmp cx,0ah    ; there is no input files at all
+      je noFileInput  
+      
+      mov bx,cx       
       xor dx,dx
       
       mov ah,2ch    ;random a numbber 0-99
@@ -101,8 +113,8 @@ startGame:
       xor ax,ax
       mov al,dl      ; dl store number 0-99
       mov cx,10
-      sub cx,bx
-      div cl
+      sub cx,bx     
+      div cl         ; cl store the num of files the program got
       xor dx,dx
       mov dl,ah      
       push dx     ; push the value of the random number (file)
@@ -114,7 +126,7 @@ startGame:
     
       call RandomFile   ;random a file and play the game
       mov ax,0                                                        
-      call CloseFile                                                        
+      call CloseFile    ; close the open file                                                    
 exit:  
       lea dx,msg6
       mov ah,9h 
@@ -124,8 +136,14 @@ exit:
       int 21h      ; get char from the user
 
       mov ah,4ch
-      int 21h      ; end the program
+      int 21h      ; end the program 
       
+noFileInput:
+      lea dx,msg10 ;print msg10
+      mov ah,09h
+      int 21h
+      
+      jmp exit      
 ;------------------------------------------------------------------       
 ; the function prints the instruction to the screen     
 proc instructions
@@ -136,18 +154,7 @@ proc instructions
       lea dx,msg8
       mov ah,9h
       int 21h   ; print msg 8
-      
-      lea dx,msg9
-      mov ah,9h
-      int 21h    ;print msg 9
-      
-      lea dx,msg10
-      mov ah,9h
-      int 21h   ;print msg 10
-      
-      lea dx,msg11 ; print msg 11
-      mov ah,9h
-      int 21h    
+         
       ret
 endp instructions
 ;-------------------------------------------------------------------
@@ -235,45 +242,46 @@ proc PlayGame
       push bp
       mov bp,sp
       mov bx,[bp+4]  ;offset of the random file
-again: 
-      push bx
+       
+      mov [FileAddres],bx 
+again:
       mov [NumOfBytes],10   ; restart NumOfBytes by 10
       
-      call getANumber   ; get a number of characters to print from the file
-              
       
-      lea dx,crlf  ;get down a line
-      mov ah,9h 
-      int 21h
+      call getANumber   ; get a number of characters to print from the file               
+                
+      mov ah,3dh     ;open a file 
+      xor al,al 
+      mov bx,[fileAddres]  ; bx has the random file offset
+      lea dx,[bx]
+      int 21h 
+      
+      jc openError  ; can't open the file
+      mov [filehandle],ax
       
       mov dh, 0    ; start print from the start of the screen
 	  mov dl, 0
 	  mov bh, 0
 	  mov ax, 0
-	  int 10h    
-	  
-	  mov ah, 00h    ; enlarge te screen to 80x25 
-      mov al, 03h
-      int 10h   
-      
-      mov ah,3dh     ;open a file 
-      xor al,al 
-      pop bx         ; bx has the random file offset
-      lea dx,[bx]
-      int 21h
-      push bx
+	  int 10h
 
-      mov [filehandle],ax
-      xor cx,cx 
+      mov ah, 00h    ; enlarge the screen to 80x25 
+      mov al, 03h
+      int 10h
+       
       cmp [mone],0   ; mone stores the number of times the program needs to print 10 bytes
       je moneEqu0    ; there is less then 10 bytes to print
-PrintAFile:      
+PrintAFile:    
+           
+            
       mov ah,3fh       ;read file
     
       mov bx,[filehandle]
       mov cx,NumOfBytes
       mov dx,offset Buffer  
-      int 21h  
+      int 21h
+      
+        
            
       lea bx,Buffer  ;put $ at the end
       add bx,ax      ;ax has the number of bytes that read from the file
@@ -291,12 +299,23 @@ PrintAFile:
       dec [mone]
       cmp [mone],0
       jnz PrintAFile ;if mone bigger then 10 the program keep printing
-moneEqu0:
+moneEqu0: 
+      cmp [sherit],0 
+      je endPrint
       mov ax,[sherit]
       mov [NumOfBytes],ax
       jmp PrintAFile    ; print only the number of bytes that sherit stores
+          
+endPrint: 
+      push ax
       
-endPrint:
+      lea dx,crlf; get down a line
+      mov ah,9h
+      int 21h 
+      
+      
+      
+GussesAgain:
       lea dx,msg3 ;print msg 3
       mov ah,9h 
       int 21h
@@ -304,10 +323,8 @@ endPrint:
       lea dx,strGuss  ; get the guss of file input into FileGuss 
       mov ah,0ah
       int 21h
-      
-        
-      
-      pop bx  ; bx has the name of the file
+            
+      mov bx,[fileAddres]  ; bx has the name of the file
       mov cx,[bp+6] ; cl has the legth of the file name
       xor ch,ch
       cmp [strGusslen],cl ; check if the guss length and file name length are equal
@@ -325,21 +342,44 @@ Gusses:
       
       lea dx,msg4  ; print msg 4
       mov ah,9h 
-      int 21h            
+      int 21h
+      
+      pop ax ;need to do pop to get the right bp            
       jmp endGame
 fail:  
       lea dx,msg5 ; print msg 5
       mov ah,9h 
       int 21h 
       
-      lea dx,strAn
+      pop ax  
+      cmp al,0      
+      je AllTheCharsShown
+      
+      lea dx,msg12  ; print msg 12
+      mov ah,9h
+      int 21h
+      
+      lea dx,strAn  ;get the user answer 
       mov ah,0ah
-      int 21h 
+      int 21h
+       
       mov ax,[Yes]
-      cmp [Answer],ax  ; check if the user want to keep playing(yes)
+      cmp [Answer],ax  ; check if the user want to keep playing(yes) 
       je again
+      jmp endGame
+      
+AllTheCharsShown:
 
-
+      lea dx,msg11 ;print msg 11
+      mov ah,09h
+      int 21h
+      
+      jmp endGame
+openError:
+      lea dx,msg13 ;print msg13
+      mov ah,09h
+      int 21h
+       
 endGame:      
       pop bp
       ret 4 
@@ -348,9 +388,11 @@ endp PlayGame
 ; the fuction gets a string from the user and convert to a number.
 ; then it divide the number by 10 and put at mone and sherit the results 
 proc getANumber
-     
+           
+getNumberAgain:  
       mov [macpil],1  ; restart the vars
       mov [result],0
+       
       lea dx,msg2   ; print msg2
       mov ah,9h 
       int 21h
@@ -358,7 +400,7 @@ proc getANumber
       lea dx,strbyte   ; put the input in bytes
       mov ah,0ah
       int 21h 
-      
+
       xor cx,cx  
       cmp [strbylen],1 ; check if the input is one digit
       je oneDigit
@@ -377,23 +419,36 @@ length:            ;mul macpil by the length of the input number
 convertToANumber:        ;make the input from string to a number
       mov al,[bytes+si]
       sub al,30h        ; make from ascii to number value
+      
+      cmp al,9          ;check if the dig is a number
+      ja invalid
+      
+      cmp al,0
+      jb invalid
       xor ah,ah
       mul [macpil]
       add [result],ax 
       mov ax,[macpil]
-      mov bx,10
+      mov bx,10           ; 10 = numofbytes
       div bx 
       mov [macpil],ax
-      inc si
+      inc si            ; the next dig
       loop convertToANumber
        
       mov cx,[result]   ; cx has the convert number
+      cmp cx, 0ffffh
+      ja invalid
       jmp moreThenOneDigit
        
 oneDigit:
       sub [bytes],30h ; make from ascii to number value
       mov cl,[bytes]
       
+      cmp cl,9
+      ja invalid
+      
+      cmp cl,0
+      jb invalid
 moreThenOneDigit: 
       add [totalBytes],cx   ; add the result to total bytes
       mov ax,[totalBytes] 
@@ -401,7 +456,13 @@ moreThenOneDigit:
       div [NumOfBytes]   ; div ax (the total bytes) by num of bytes(10) 
       mov [mone],ax     ; mone gets the portion
       mov [sherit],dx   ; mone gets the remainder  
-      ret
+      ret 
+invalid:
+      lea dx,msg9
+      mov ah,09h
+      int 21h
+      
+      jmp getNumberAgain
 endp getANumber
 ;----------------------------------------------------------------- 
 ; the function close the open file
@@ -413,4 +474,6 @@ proc CloseFile
 endp CloseFile
 
 end
+
+
 
